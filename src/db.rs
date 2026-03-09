76cb358bd540a394ece306ad41f7e857fe11bc1f
gabriel <gabriel.leonardo.cruz@outlook.com>
2026-03-09 16:32:54 -0300

    adcionei db pools!

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,16 @@
 use std::env;
 use dotenvy::dotenv;
 use diesel::prelude::*;
+ use diesel::r2d2::{Pool,ConnectionManager};
 
-pub fn establish_connection() -> SqliteConnection {
+pub type DbPool = Pool<ConnectionManager<SqliteConnection>>;
+
+pub fn establish_connection_pool() -> DbPool {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL não encontrado!");
-    SqliteConnection::establish(&database_url).expect("erro ao conectar com o banco de dados!")
+    let database_url = env::var("DATABASE_URL").expect("erro ao encontrar database_url");
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+
+    Pool::builder()
+        .build(manager)
+        .expect("erro ao criar a database pool")
 }
